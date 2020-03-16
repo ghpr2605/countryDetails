79787d7c495518cd00d45d94e89bdf38382c90b0
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2020-03-16 16:49:24 +0530

    Added changes to app.js for optimized logic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var countriesList = [];
+let countriesList = [];
 
 window.onload = () => {
     const countriesSelectEl = document.getElementById('countrySelect');
@@ -10,10 +10,33 @@ window.onload = () => {
             countriesList = data;
             console.log(data);
             data.forEach(country => {
+                const { alpha2Code, name } = country;
                 const option = document.createElement('option');
-                option.value = country.alpha2Code;
-                option.innerText = country.name;
+                option.value = alpha2Code;
+                option.innerText = name;
                 countriesSelectEl.appendChild(option);
             });
         })  
+}
+
+function select(el){
+    const countryCode = el.value;
+    const contentEl = document.getElementById('content');
+    const selectedCountry = countriesList.find(c => c.alpha2Code === countryCode);
+    if(selectedCountry){
+        const {flag, capital, population, currencies, callingCodes} = selectedCountry;
+        const content = `
+        <div>
+            <img src="${flag}" class="card front"/>
+            <div class="card back padding">
+                <h3><li><u>Capital:</u> ${capital}</h3>
+                <h3><li><u>Population:</u> ${population}</h3>
+                <h3><li><u>Currency:</u> ${currencies[0].name}</h3>
+                <h3><li><u>Currency Symbol:</u> ${currencies[0].symbol}</h3>
+                <h3><li><u>Phone code:</u> +${callingCodes}</h3>
+            </div>
+        </div>   
+      `;
+      contentEl.innerHTML = content;
+    }
 }
